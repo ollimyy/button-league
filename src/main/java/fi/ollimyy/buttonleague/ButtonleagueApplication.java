81c4803f1eb5817ebb0e15a2a9c58e26e09df8fa
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-03-18 10:06:16 +0200

    Initial commit

---
+++ src/main/java/fi/ollimyy/buttonleague/ButtonleagueApplication.java
@@ -0,0 +1,13 @@
+package fi.ollimyy.buttonleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtonleagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButtonleagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/ollimyy/buttonleague/ButtonleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.ollimyy.buttonleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButtonleagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
