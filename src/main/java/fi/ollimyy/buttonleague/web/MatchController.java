7ed68e7fe1df1645368be25a5daaede05ce4f3bf
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-03-27 20:18:50 +0300

    Add ability to add new matches.
    Users with the role admin can now add new matches with the add-match form.

--- src/main/java/fi/ollimyy/buttonleague/web/MatchController.java
+++ src/main/java/fi/ollimyy/buttonleague/web/MatchController.java
@@ -2,11 +2,14 @@ package fi.ollimyy.buttonleague.web;
 
 import fi.ollimyy.buttonleague.domain.Match;
 import fi.ollimyy.buttonleague.domain.MatchRepository;
+import fi.ollimyy.buttonleague.domain.TeamRepository;
 import fi.ollimyy.buttonleague.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,6 +22,9 @@ public class MatchController {
     MatchRepository matchRepository;
 
     @Autowired
+    TeamRepository teamRepository;
+
+    @Autowired
     MatchService matchService;
 
     @GetMapping("/match-list")
@@ -26,5 +32,21 @@ public class MatchController {
         Map<LocalDate, List<Match>> matchesByDate = matchService.getMatchesGroupedByDate();
         model.addAttribute("matchesByDate", matchesByDate);
         return "match-list";
+    }
+
+    @GetMapping("/add-match")
+    @PreAuthorize("hasAuthority('admin')")
+    public String addMatch(Model model) {
+        model.addAttribute("match", new Match());
+        model.addAttribute("teams", teamRepository.findAll());
+
+        return "add-match";
+    }
+
+    @PostMapping("/save-match")
+    public String saveMatch(Match match) {
+        matchRepository.save(match);
+
+        return "redirect:match-list";
     }
 }
