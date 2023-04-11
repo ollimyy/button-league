d5a4a9ee238dc34ae4e0fd57a44af9a01a102726
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-04-11 16:56:21 +0300

    Change delete team button to be a link instead of a button and simplify the javascript.

--- src/main/resources/static/js/team-page-script.js
+++ src/main/resources/static/js/team-page-script.js
@@ -10,16 +10,4 @@ window.addEventListener('load', function () {
         editButton.style.display = "none";
         editForm.style.display = "block";
     });
-
-    // clicking delete team shows an "are you sure" alert, if user clicks yes team and it's matches and players will be deleted
-    const deleteTeamButton = document.getElementById("delete-team-button");
-
-    deleteTeamButton.addEventListener("click", function () {
-        const confirmed = confirm("Deleting this team will delete all it's players and all played and upcoming matches the team is in." +
-            "\n\nAre you sure you want to delete this team AND all it's players and matches?")
-        if (confirmed) {
-            const teamId = document.getElementById("team-id").value;
-            window.location.href = "/delete-team/" + teamId;
-        }
-    })
 });
