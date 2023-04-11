2d18b52b85205719ef10ed8a35d82a93870889c3
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-04-11 16:23:49 +0300

    Add delete player feature to admin tools.

--- src/main/java/fi/ollimyy/buttonleague/web/PlayerController.java
+++ src/main/java/fi/ollimyy/buttonleague/web/PlayerController.java
@@ -3,8 +3,12 @@ package fi.ollimyy.buttonleague.web;
 import fi.ollimyy.buttonleague.domain.Player;
 import fi.ollimyy.buttonleague.domain.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class PlayerController {
@@ -13,9 +17,18 @@ public class PlayerController {
     PlayerRepository playerRepository;
 
     @PostMapping("/save-player")
+    @Secured("ADMIN")
     public String savePlayer(Player player) {
         playerRepository.save(player);
 
         return "redirect:/team/" + player.getTeam().getId();
+    }
+
+    @GetMapping("/delete-player/{playerId}")
+    @Secured("ADMIN")
+    public String deletePlayer(@PathVariable("playerId") Long playerId, @RequestHeader String referer) {
+        playerRepository.deleteById(playerId);
+
+        return "redirect:" + referer;
     }
 }
