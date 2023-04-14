d851caad741d2d308651dea06014855220384cb2
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-04-14 11:23:37 +0300

    Fix css not available to non-authenticated users.

--- src/main/java/fi/ollimyy/buttonleague/WebSecurityConfig.java
+++ src/main/java/fi/ollimyy/buttonleague/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/team-list", "/team/**", "/league-table", "/match-list").permitAll()
+                .requestMatchers("/team-list", "/team/**", "/league-table", "/match-list", "/css/**").permitAll()
                 .anyRequest().authenticated()
              .and()
                 .formLogin()
